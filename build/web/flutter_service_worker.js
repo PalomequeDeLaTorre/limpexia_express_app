37a2d853b3f9015e847453b6702eaa6e2e5b788d
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2025-11-01 22:10:03 -0600

    Version_Pantallas

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c1190c93a57b3f977e81bd7de208a80c
 "credenciales.js": "d41d8cd98f00b204e9800998ecf8427e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "21ab514b87ebcfcb948d630140f71ca0",
+"flutter_bootstrap.js": "60c8f7a97db424ee0d542d86efba9c92",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
