dd007e73560836d8a8fd3542cd506575b755238b
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2025-11-27 00:27:59 -0600

    version1.5

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,36 +7,24 @@ import Foundation
 
 import cloud_firestore
 import connectivity_plus
-<<<<<<< HEAD
 import file_selector_macos
-=======
->>>>>>> b9ac6c1372d29ac468c4569198191fe108a9ef47
 import firebase_auth
 import firebase_core
 import firebase_database
 import firebase_messaging
-<<<<<<< HEAD
 import firebase_storage
-=======
->>>>>>> b9ac6c1372d29ac468c4569198191fe108a9ef47
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
-<<<<<<< HEAD
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=======
->>>>>>> b9ac6c1372d29ac468c4569198191fe108a9ef47
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-<<<<<<< HEAD
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-=======
->>>>>>> b9ac6c1372d29ac468c4569198191fe108a9ef47
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
